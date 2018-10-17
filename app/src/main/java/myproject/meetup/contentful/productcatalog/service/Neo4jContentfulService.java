34f5028684627ced7b81a60d927c99dbe49ae12b
guangjun <yangguangjun123@gmail.com>
2018-10-17 21:05:17 +0100

    Development of contentful Neo4j service class

--- app/src/main/java/myproject/meetup/contentful/productcatalog/service/Neo4jContentfulService.java
+++ app/src/main/java/myproject/meetup/contentful/productcatalog/service/Neo4jContentfulService.java
@@ -7,9 +7,6 @@ import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.Transaction;
-import org.neo4j.driver.v1.TransactionWork;
-import org.neo4j.driver.v1.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +19,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
@@ -45,6 +40,7 @@ public class Neo4jContentfulService {
     public void init() {
         driver = GraphDatabase.driver(neo4jProperties.getDburl(), AuthTokens.basic(neo4jProperties.getDbuser(),
                 neo4jProperties.getDbpassword()));
+        deleteAll();
     }
 
     @PreDestroy
@@ -63,10 +59,6 @@ public class Neo4jContentfulService {
         JSONArray jsonArray = new JSONArray(jsonEntryArrayString);
         logger.info("size of json array: " + jsonArray.length());
         jsonArray.toList().stream()
-//                .peek(s -> logger.info("peek1: " + s.getClass().getCanonicalName()))
-//                .filter(json -> json instanceof JSONObject)
-//                    .peek(s -> logger.info("peek2: " + Objects.toString(s)))
-//                          .map(JSONObject.class::cast)
                           .map(HashMap.class::cast)
                           .filter(jsonMap -> Objects.isNull(jsonMap.get("archived")) ||
                                   !Boolean.valueOf(jsonMap.get("archived").toString()))
@@ -76,9 +68,6 @@ public class Neo4jContentfulService {
     }
 
     private void processContentfulJSONMap(HashMap<String, Object> contentfulMap) {
-//        String spaceId = (String) contentfulMap.get("spaceId");
-//        String id = (String) contentfulMap.get("id");
-
         // process system map
         contentfulMap.entrySet()
                      .stream()
@@ -111,74 +100,121 @@ public class Neo4jContentfulService {
         List<String> cypherStatements = new ArrayList<>();
         String type = (String) systemMap.get("type");
         String id = (String) systemMap.get("id");
-        LocalDateTime createdAt = LocalDateTime.parse((String) systemMap.get("createdAt"),
-                DateTimeFormatter.ISO_INSTANT);
-        LocalDateTime updatedAt = LocalDateTime.parse((String) systemMap.get("updatedAt"),
-                DateTimeFormatter.ISO_INSTANT);
-        LocalDateTime publishedAt = LocalDateTime.parse((String) systemMap.get("publishedAt"),
-                DateTimeFormatter.ISO_INSTANT);
-        cypherStatements.add("CREATE (n:$type, { id = $id, createdAt = $createdAt, updatedBy = $updatedAt, " +
-                "publishedAt = $published }) ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS[X]");
+        LocalDateTime createdAt = LocalDateTime.parse((String) systemMap.get("createdAt"), formatter);
+        LocalDateTime updatedAt = LocalDateTime.parse((String) systemMap.get("updatedAt"), formatter);
+        LocalDateTime publishedAt = LocalDateTime.parse((String) systemMap.get("publishedAt"), formatter);
+        String entityNode = ("CREATE (n:#label# { id : {id} }) SET n.createdAt = $createdAt, " +
+                "n.updatedBy = $updatedAt, " + "n.publishedAt = $publishedAt");
+        try( Session session = driver.session()) {
+            session.run(entityNode.replace("#label#", type), parameters( "id", id,
+                    "createdAt", createdAt, "updatedAt", updatedAt, "publishedAt", publishedAt ));
+        }
 
-        JSONObject contentTypeJSON = (JSONObject) systemMap.get("contentType");
+        JSONObject contentTypeJSON = systemJSON.getJSONObject("contentType");
         String contentTypeId = contentTypeJSON.query("/system/id").toString();
-        boolean contentTypeArchived = Boolean.valueOf(contentTypeJSON.getString("archived"));
-        boolean contentTypePublished = Boolean.valueOf((String) contentTypeJSON.query("/system/published"));
+        boolean contentTypeArchived = contentTypeJSON.getBoolean("archived");
+        boolean contentTypePublished = contentTypeJSON.getBoolean("published");
         String contentTypeRelation = contentTypeJSON.query("/system/type").toString();
         String contentTypeLinkType = contentTypeJSON.query("/system/linkType").toString();
-        cypherStatements.add("MERGE (n:$contentTypeLinkType, { id = $contentTypeId }) ON CREATE SET " +
-                "archived = $contentTypeArchived, published = $contentTypePublished");
-        cypherStatements.add("MATCH(n:$type, { id = $id }) " +
-                             "MATCH(m:contentTypeLinkType, { id = $contentTypeId )" +
-                             "CREATE (n)-[:$contentTypeRelation, { linkType: $contentTypeLinkType }]-(m)");
+        String contentTypeNode = "MERGE (n:#contentTypeLinkType# { id : {contentTypeId} }) ON CREATE SET " +
+                "n.archived = $contentTypeArchived, n.published = $contentTypePublished";
+        String contentTypeRelationship = ("MATCH(n:#type# { id : {id} }) " +
+                             "MATCH(m:#contentTypeLinkType# { id : {contentTypeId} }) " +
+                             "CREATE (n)-[:#contentTypeRelation# { linkType: {contentTypeLinkType} }]->(m)");
 
-        // createdBy
-        JSONObject createdByJSON = (JSONObject) systemMap.get("createdBy");
-        String createdById = contentTypeJSON.query("/system/id").toString();
-        boolean createdByArchived = Boolean.valueOf(createdByJSON.getString("archived"));
-        boolean createdByPublished = Boolean.valueOf((String) createdByJSON.query("/system/published"));
+        try( Session session = driver.session()) {
+            session.run(contentTypeNode.replace("#contentTypeLinkType#", contentTypeLinkType),
+                    parameters( "contentTypeId", contentTypeId,
+                            "contentTypeArchived", contentTypeArchived,
+                            "contentTypePublished", contentTypePublished));
+
+            session.run(contentTypeRelationship.replace("#type#", type)
+                                               .replace("#contentTypeLinkType#", contentTypeLinkType)
+                                               .replace("#contentTypeRelation#", contentTypeRelation),
+                    parameters( "id", id,
+                            "contentTypeId", contentTypeId,
+                            "contentTypeLinkType", contentTypeLinkType));
+        }
+
+        // createdBypublishedByPublished
+        JSONObject createdByJSON = systemJSON.getJSONObject("createdBy");
+        String createdById = createdByJSON.query("/system/id").toString();
+        boolean createdByArchived = createdByJSON.getBoolean("archived");
+        boolean createdByPublished = createdByJSON.getBoolean("published");
         String createdByRelation = createdByJSON.query("/system/type").toString();
         String createdByLinkType = createdByJSON.query("/system/linkType").toString();
-        cypherStatements.add("MERGE (n:${createdByLinkType}, { id = $cratedById }) ON CREATE SET " +
-                "archived = $createdByArchived, published = $createdByPublished");
-        cypherStatements.add("MATCH(n:$type, { id = $id }) " +
-                "MATCH(m:$createdByLinkType, { id = $createdById })" +
-                "CREATE (n)-[:${createdByRelation}, { linkType: $createdByLinkType }]-(m)");
+        String createdByNode = "MERGE (n:#createdByLinkType# { id : {createdById} }) ON CREATE SET " +
+                "n.archived = $createdByArchived, n.published = $createdByPublished";
+        String createdByRelationship = "MATCH(n:#type# { id : $id }) " +
+                "MATCH(m:#createdByLinkType# { id : {createdById} })" +
+                "CREATE (n)-[:#createdByRelation# { linkType: {createdByLinkType} }]->(m)";
+        try( Session session = driver.session()) {
+            session.run(createdByNode.replace("#createdByLinkType#", createdByLinkType),
+                    parameters( "createdById", createdById,
+                            "createdByArchived", createdByArchived,
+                            "createdByPublished", createdByPublished));
 
-        // publishedBy
-        JSONObject updatedByJSON = (JSONObject) systemMap.get("updatedBy");
-        String updatedById = contentTypeJSON.query("/system/id").toString();
-        boolean updatedByArchived = Boolean.valueOf(updatedByJSON.getString("archived"));
-        boolean updatedByPublished = Boolean.valueOf((String) updatedByJSON.query("/system/published"));
+            session.run(createdByRelationship.replace("#type#", type)
+                            .replace("#createdByLinkType#", createdByLinkType)
+                            .replace("#createdByRelation#", createdByRelation),
+                    parameters( "id", id,
+                            "createdById", createdById,
+                            "createdByLinkType", createdByLinkType));
+        }
+
+        // updatedBy
+        JSONObject updatedByJSON = systemJSON.getJSONObject("updatedBy");
+        String updatedById = updatedByJSON.query("/system/id").toString();
+        boolean updatedByArchived = updatedByJSON.getBoolean("archived");
+        boolean updatedByPublished = updatedByJSON.getBoolean("published");
         String updatedByRelation = updatedByJSON.query("/system/type").toString();
         String updatedByLinkType = updatedByJSON.query("/system/linkType").toString();
-        cypherStatements.add("MERGE (n:$updatedByLinkType, { id = $cratedById }) ON CREATE SET " +
-                "archived = $updatedByArchived, published = $updatedByPublished");
-        cypherStatements.add("MATCH(n:$type, { id = $id }) " +
-                "MATCH(m:$updatedByLinkType, { id = $updatedById })" +
-                "CREATE (n)-[:${updatedByRelation}, { linkType: $updatedByLinkType }]-(m)");
+        String updatedByNode = "MERGE (n:#updatedByLinkType# { id : {updatedById} }) ON CREATE SET " +
+                "n.archived = $updatedByArchived, n.published = $updatedByPublished";
+        String updatedByRelationship = "MATCH(n:#type# { id : {id} }) " +
+                "MATCH(m:#updatedByLinkType# { id : {updatedById} })" +
+                "CREATE (n)-[:#updatedByRelation# { linkType: {updatedByLinkType} }]->(m)";
+        try( Session session = driver.session()) {
+            session.run(updatedByNode.replace("#updatedByLinkType#", updatedByLinkType),
+                    parameters( "updatedById", updatedById,
+                            "updatedByArchived", updatedByArchived,
+                            "updatedByPublished", updatedByPublished));
 
-        // published
-        JSONObject publishedByJSON = (JSONObject) systemMap.get("publishedBy");
-        String publishedById = contentTypeJSON.query("/system/id").toString();
-        boolean publishedByArchived = Boolean.valueOf(publishedByJSON.getString("archived"));
-        boolean publishedByPublished = Boolean.valueOf((String) publishedByJSON.query("/system/published"));
+            session.run(updatedByRelationship.replace("#type#", type)
+                            .replace("#updatedByLinkType#", updatedByLinkType)
+                            .replace("#updatedByRelation#", updatedByRelation),
+                    parameters( "id", id,
+                            "updatedById", updatedById,
+                            "updatedByLinkType", updatedByLinkType));
+        }
+
+        // publishedBy
+        JSONObject publishedByJSON = systemJSON.getJSONObject("publishedBy");
+        String publishedById = publishedByJSON.query("/system/id").toString();
+        boolean publishedByArchived = publishedByJSON.getBoolean("archived");
+        boolean publishedByPublished = publishedByJSON.getBoolean("published");
         String publishedByRelation = publishedByJSON.query("/system/type").toString();
         String publishedByLinkType = publishedByJSON.query("/system/linkType").toString();
-        cypherStatements.add("MERGE (n:$publishedByLinkType, { id = $cratedById }) ON CREATE SET " +
-                "archived = $publishedByArchived, published = $publishedByPublished");
-        cypherStatements.add("MATCH(n:$type, { id = $id }) " +
-                "MATCH(m:publishedByLinkType, { id = ${publishedById })" +
-                "CREATE (n)-[:$publishedByRelation, { linkType: $publishedByLinkType }]-(m)");
+        String publishedByNode = "MERGE (n:#publishedByLinkType# { id : {publishedById} }) ON CREATE SET " +
+                "n.archived = $publishedByArchived, n.published = $publishedByPublished";
+        String publishedByRelationship = "MATCH(n:#type# { id : {id} }) " +
+                "MATCH(m:#publishedByLinkType# { id : {publishedById} })" +
+                "CREATE (n)-[:#publishedByRelation# { linkType: {publishedByLinkType} }]->(m)";
 
-        try ( Session session = driver.session() ) {
-            session.writeTransaction( new TransactionWork<Optional<Object>>() {
-                @Override
-                public Optional<Object> execute(Transaction tx ) {
-                    cypherStatements.forEach( s -> tx.run(s));
-                    return Optional.empty();
-                }
-            } );
+        try( Session session = driver.session()) {
+            session.run(publishedByNode.replace("#publishedByLinkType#", publishedByLinkType),
+                    parameters( "publishedById", publishedById,
+                            "publishedByArchived", publishedByArchived,
+                            "publishedByPublished", publishedByPublished));
+
+            session.run(publishedByRelationship.replace("#type#", type)
+                            .replace("#publishedByLinkType#", publishedByLinkType)
+                            .replace("#publishedByRelation#", publishedByRelation),
+                    parameters( "id", id,
+                            "publishedById", publishedById,
+                            "publishedByLinkType", publishedByLinkType));
         }
+
     }
 }
