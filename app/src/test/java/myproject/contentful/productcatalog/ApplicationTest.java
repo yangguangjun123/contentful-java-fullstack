dafb3db6a5c11ad16df3ebac45bd9f2d86950b8d
guangjun <yangguangjun123@gmail.com>
2020-02-27 22:00:59 +0000

    Refactor code and test environment

--- app/src/test/java/myproject/contentful/productcatalog/ApplicationTest.java
+++ app/src/test/java/myproject/contentful/productcatalog/ApplicationTest.java
@@ -4,13 +4,11 @@ import myproject.contentful.productcatalog.api.ContentfulNeo4jController;
 import myproject.contentful.productcatalog.service.ContentfulService;
 import myproject.contentful.productcatalog.service.Neo4jDatabaseService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
--- app/src/test/java/myproject/contentful/productcatalog/api/ContentfulControllerTest.java
+++ app/src/test/java/myproject/contentful/productcatalog/api/ContentfulControllerTest.java
@@ -1,39 +1,41 @@
 package myproject.contentful.productcatalog.api;
 
 import myproject.contentful.productcatalog.service.ContentfulService;
-import myproject.contentful.productcatalog.service.Neo4jDatabaseService;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ContentfulController.class)
 public class ContentfulControllerTest {
-
     @MockBean
     private ContentfulService contentfulService;
-
-    @InjectMocks
-    private ContentfulController contentfulController;
 
 //    @Autowired
 //    private String spaceId;
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(contentfulController).build();
-    }
+    private static final Logger logger = LoggerFactory.getLogger(ContentfulControllerTest.class);
 
     @Test
-    public void shouldDeleteContentfulSpaceById() {
-        // given
+    public void shouldDeleteContentfulSpaceById() throws Exception {
+        logger.info("running shouldDeleteContentfulSpaceById");
 
-        // when
-
+        mockMvc.perform(put("/contentful/space/updateCach")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("success")));
+        verify(contentfulService, times(1)).init();
     }
 }
